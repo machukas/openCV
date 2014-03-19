864899bb975b401f92a072092622fab15daf1106
machukas <machukkas@gmail.com>
2014-03-19 08:14:56 +0100

    Practica 2

---
+++ P2/main.h
@@ -0,0 +1,14 @@
+//
+//  main.h
+//  Vision por computador
+//
+//  Created by Nicolas Landa Tejero-Garces on 19/03/14.
+//  Copyright (c) 2014 Adrian Marin Colas. All rights reserved.
+//
+
+#ifndef __Vision_por_computador__main__
+#define __Vision_por_computador__main__
+
+#include <iostream>
+
+#endif /* defined(__Vision_por_computador__main__) */
